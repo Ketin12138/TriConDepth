4120b8d49a20511ab1150249815cf8f51d7ca1ba
Ketin12138 <zhongyh39@mail2.sysu.edu.cn>
2026-08-11 15:09:37 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from .pretrain_phase1 import Stage1Model
+from .pretrain_phase2 import Stage2Model
+from .pretrain_phase3 import Stage3Model
+from .pretrain_phase4 import Stage4Model
+from .pretrain_phase4mix import Stage4MixModel
+from .baseline import BaselineModel
+from .meanteacher import MeanTeacher
+from .fixmatch import FixMatch
+from .clss import CLSS
+
+from .registry import MODELS
