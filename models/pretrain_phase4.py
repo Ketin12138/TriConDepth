112f54666d41f78e3a33043c652ee6f38ff9f959
Ketin12138 <zhongyh39@mail2.sysu.edu.cn>
2026-08-11 15:12:05 +0800

    Update pretrain_phase4.py

--- models/pretrain_phase4.py
+++ models/pretrain_phase4.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from networks.DCDepth import DCDepth
 from networks.YHDepth import YHDepth
 from pytorch_lightning import LightningModule
 from torch.optim import AdamW
